ae6bf109d33e60163d5dfa45d602355a00ecef5a
Nasar <shaiknasar765@gmail.com>
2021-09-03 12:35:50 +0530

    version 3

---
+++ contact2.php
@@ -0,0 +1,29 @@
+<?php
+
+$name=$_POST['name'];
+$number=$_POST['number'];
+$email=$_POST['email'];
+$message=$_POST['message'];
+
+$conn=new mysqli('localhost','root','','contact');
+if($conn->connect_error){
+    die('Connection Failed :'. $conn->connect_error);
+    
+}else{
+    $stmt=$conn->prepare("insert into students (Name,Number,Email,Message) values(?,?,?,?)");
+    $stmt->bind_param("siss",$name,$number,$email,$message);
+    $stmt->execute();
+    $message="Thanks for your interest we will reach out you in shortly";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    echo " - 0  Message : Registration Successful.";
+    echo "<script>window.location.replace('index.php');</script>";
+    echo "<a href='index.html'>Go to Home</a>";
+
+
+    $stmt->close();
+    $conn->close();
+    
+    
+}
+
+?>
